ba4c294ca1a35ea1d37023e625fa6a20dd0cac3b
Dat Nguyen <lonelyicevalley@icloud.com>
2020-04-03 17:46:06 +0700

    hw2 q4 LQR controller

--- hw2/lqr_infinite_horizon_solution.m
+++ hw2/lqr_infinite_horizon_solution.m
@@ -6,7 +6,25 @@ dt = 0.1;
 mc = 10; mp = 2.; l = 1.; g= 9.81;
 
 % TODO write A,B matrices
+a1 = mp*g/mc;
+a2 = (mc + mp)*g/(l*mc);
+df_ds = [0 0 1 0; 0 0 0 1; 0 a1 0 0; 0 a2 0 0];
+df_du = [0; 0; 1/mc; 1/(l*mc)];
+A = eye(4) + dt*df_ds;
+B = dt*df_du;
 
 % TODO implement Riccati recursion
-
+L_current = 1e9*ones(1,4);
+P = eye(4);
+Q = eye(4);
+R = 1;
+while true
+    L_new = -inv(R + B'*P*B)*(B'*P*A);
+    P = Q + L_new'*R*L_new + (A + B*L_new)'*P*(A + B*L_new);
+    if norm(L_new - L_current) <= 1e-4
+        break
+    end
+    L_current = L_new;
+end
+L = L_current;
 end
