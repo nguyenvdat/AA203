9983e12f85fed69db237bc481c80fa78e35346d7
Dat Nguyen <lonelyicevalley@icloud.com>
2020-04-09 22:15:57 +0700

    hw6 q2 direct method with trapezoidal rule

--- hw6/Trapezoidal/ChemicalReaction/constraint.m
+++ hw6/Trapezoidal/ChemicalReaction/constraint.m
@@ -9,20 +9,27 @@ global T;
 global x0;
 global y0;
 
-% Put here constraint inequalities
-c = ;
-
 % Note that var = [x;y;u]
 x = var(1:N+1); y = var(N+2:2*N+2); u = var(2*N+3:3*N+3);
+
+% Put here constraint inequalities
+for i = 1:N+1
+    c(i) = x(i) - 1;
+    c(i+N+1) = y(i) - 1;
+    c(i+2*(N+1)) = u(i) - 1;
+    c(i+3*(N+1)) = -x(i);
+    c(i+4*(N+1)) = -y(i);
+    c(i+5*(N+1)) = -u(i);
+end
 
 % Computing dynamical constraints via the trapezoidal rule
 h = 1.0*T/(1.0*N);
 for i = 1:N
     % Provide here dynamical constraints via the trapeziodal formula
-    ceq(i) = ;
-    ceq(i+N) = ;
+    ceq(i) = x(i+1) - x(i) - h*(-x(i)*u(i)+y(i)*u(i)*u(i) - x(i+1)*u(i+1)+y(i+1)*u(i+1)*u(i+1))/2;
+    ceq(i+N) = y(i+1) - y(i) - h*(x(i)*u(i)-3*y(i)*u(i)*u(i) + x(i+1)*u(i+1)-3*y(i+1)*u(i+1)*u(i+1))/2;
 end
 
 % Put here initial conditions
-ceq(1+2*N) = ;
-ceq(2+2*N) = ;
+ceq(1+2*N) = x(1)-1;
+ceq(2+2*N) = y(1);
